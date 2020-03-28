8bd19a95a7981354f91d078cf003394e79a57757
Florian Leroux <florian.leroux3@laposte.net>
2020-03-28 19:16:50 +0100

    Initial Commit

---
+++ assets/js/nav.js
@@ -0,0 +1,37 @@
+$('#body').append('<nav class="navbar navbar-expand-lg navbar-dark bg-dark">\n' +
+    '    <a class="navbar-brand d-flex" href="/index.html">\n' +
+    '        <img src="assets/img/js.png" width="30" height="30" alt="Hello JS">\n' +
+    '        <span class="align-content-center ml-3">Hello JS</span>\n' +
+    '\n' +
+    '    </a>\n' +
+    '    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"\n' +
+    '            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\n' +
+    '        <span class="navbar-toggler-icon"></span>\n' +
+    '    </button>\n' +
+    '\n' +
+    '    <div class="collapse navbar-collapse" id="navbarSupportedContent">\n' +
+    '        <ul class="navbar-nav mr-auto">\n' +
+    '            <li class="nav-item active">\n' +
+    '                <a class="nav-link" href="index.html">Accueil</a>\n' +
+    '            </li>\n' +
+    '            <li class="nav-item">\n' +
+    '                <a class="nav-link" href="#">Lien</a>\n' +
+    '            </li>\n' +
+    '            <li class="nav-item dropdown">\n' +
+    '                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"\n' +
+    '                   aria-haspopup="true" aria-expanded="false">\n' +
+    '                    Menu\n' +
+    '                </a>\n' +
+    '                <div class="dropdown-menu" aria-labelledby="navbarDropdown">\n' +
+    '                    <a class="dropdown-item" href="#">Action</a>\n' +
+    '                    <a class="dropdown-item" href="#">Autre action</a>\n' +
+    '                    <div class="dropdown-divider"></div>\n' +
+    '                    <a class="dropdown-item" href="#">Encore une autre action</a>\n' +
+    '                </div>\n' +
+    '            </li>\n' +
+    '        </ul>\n' +
+    '        <div class="form-inline my-2 my-lg-0">\n' +
+    '            <input class="form-control mr-sm-2" id="search" placeholder="Recherche">\n' +
+    '        </div>\n' +
+    '    </div>\n' +
+    '</nav>\n');
