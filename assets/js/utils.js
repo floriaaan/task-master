80cc9e42a7d1008f6c8b758bf403671a1c8a25cb
Florian Leroux <florian.leroux3@laposte.net>
2020-04-01 16:41:39 +0200

    clear local

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -26,10 +26,8 @@ async function init() {
         });
         fetchNextPage();
     }).then(function() {
-        console.log(memberList);
         for (let i = 0; i < memberList.length; i++) {
             let member = new Member(memberList[i].fields.name, memberList[i].fields.role);
-            console.log(member);
             member.id = memberList[i].id;
             member.save();
         }
